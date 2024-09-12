6df87812877ef37c76be993b64213111e4001e34
Guilherme Philippsen de Souza <97970573+guiphilippsen@users.noreply.github.com>
2024-09-12 09:00:43 -0300

    Add files via upload

---
+++ src/main/java/com/github/guiphilippsen/api_votos/ApiVotosApplication.java
@@ -0,0 +1,13 @@
+package com.github.guiphilippsen.api_votos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/guiphilippsen/api_votos/ApiVotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guiphilippsen.api_votos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
